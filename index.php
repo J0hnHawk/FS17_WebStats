9400bb3cfd81994f3bc748faf3868c7379f42a03
Lars Bleckwenn <lars.bleckwenn@web.de>
2018-01-26 16:41:48 +0100

    Added build number

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 ini_set ( 'error_reporting', E_ALL );
-ini_set ( 'display_errors', 1 );
+ini_set ( 'display_errors', 0 );
 ini_set ( 'log_errors', 1 );
 ini_set ( 'error_log', 'error.log' );
 
@@ -42,10 +42,10 @@ include ('./include/coockie.php');
 $style = $options ['general'] ['style'];
 
 $smarty = new Smarty ();
-$smarty->debugging = true;
+$smarty->debugging = false;
 $smarty->caching = false;
 $smarty->setTemplateDir ( "./styles/$style/templates" );
-$smarty->assign ( 'webStatsVersion', 'Version 1.4.0 (alpha)' );
+$smarty->assign ( 'webStatsVersion', '1.4.0-781 (26.01.2018)' );
 
 include ('./include/loadConfig.php');
 
