9e6f4d88772a76eaf6a462d26d0b47c421ba375f
Lars Bleckwenn <lars.bleckwenn@web.de>
2018-01-26 17:09:33 +0100

    Czech translation for game own fill types

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $smarty = new Smarty ();
 $smarty->debugging = false;
 $smarty->caching = false;
 $smarty->setTemplateDir ( "./styles/$style/templates" );
-$smarty->assign ( 'webStatsVersion', '1.4.0-781 (26.01.2018)' );
+$smarty->assign ( 'webStatsVersion', '1.4.0-782 (26.01.2018)' );
 
 include ('./include/loadConfig.php');
 
