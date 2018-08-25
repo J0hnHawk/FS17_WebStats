5147f844673b393323875759844367d9f4b0c426
Lars Bleckwenn <lars.bleckwenn@web.de>
2018-08-25 22:24:00 +0200

    Changed data structure

--- include/finances.php
+++ include/finances.php
@@ -1,117 +1,48 @@
 <?php
 $financeElements = array (
-		'newVehiclesCost' => array (
-				- 1,
-				1 
-		),
-		'soldVehicles' => array (
-				1,
-				9 
-		),
-		'newAnimalsCost' => array (
-				- 1,
-				2 
-		),
-		'soldAnimals' => array (
-				1,
-				9 
-		),
-		'constructionCost' => array (
-				- 1,
-				3 
-		),
-		'soldBuildings' => array (
-				1,
-				9 
-		),
-		'fieldPurchase' => array (
-				- 1,
-				3 
-		),
-		'vehicleRunningCost' => array (
-				- 1,
-				4 
-		),
-		'vehicleLeasingCost' => array (
-				- 1,
-				4 
-		),
-		'animalUpkeep' => array (
-				- 1,
-				2 
-		),
-		'propertyMaintenance' => array (
-				- 1,
-				5 
-		),
-		'propertyIncome' => array (
-				1,
-				9 
-		),
-		'soldWood' => array (
-				1,
-				9 
-		),
-		'soldBales' => array (
-				1,
-				9 
-		),
-		'soldWool' => array (
-				1,
-				9 
-		),
-		'soldMilk' => array (
-				1,
-				9 
-		),
-		'purchaseFuel' => array (
-				- 1,
-				4 
-		),
-		'purchaseSeeds' => array (
-				- 1,
-				9 
-		),
-		'purchaseFertilizer' => array (
-				- 1,
-				9 
-		),
-		'purchaseSaplings' => array (
-				- 1,
-				9 
-		),
-		'purchaseWater' => array (
-				- 1,
-				9 
-		),
-		'harvestIncome' => array (
-				1,
-				7 
-		),
-		'incomeBga' => array (
-				1,
-				7 
-		),
-		'missionIncome' => array (
-				1,
-				8 
-		),
-		'wagePayment' => array (
-				- 1,
-				6 
-		),
-		'other' => array (
-				- 1,
-				9 
-		),
-		'loanInterest' => array (
-				- 1,
-				10 
-		) 
+		'newVehiclesCost' => 0,
+		'soldVehicles' => 0,
+		'newAnimalsCost' => 1,
+		'soldAnimals' => 1,
+		'constructionCost' => 2,
+		'soldBuildings' => 2,
+		'fieldPurchase' => 2,
+		'vehicleRunningCost' => 3,
+		'vehicleLeasingCost' => 3,
+		'animalUpkeep' => 1,
+		'propertyMaintenance' => 4,
+		'propertyIncome' => 4,
+		'soldWood' => 6,
+		'soldBales' => 6,
+		'soldWool' => 1,
+		'soldMilk' => 1,
+		'purchaseFuel' => 0,
+		'purchaseSeeds' => 8,
+		'purchaseFertilizer' => 8,
+		'purchaseSaplings' => 8,
+		'purchaseWater' => 8,
+		'harvestIncome' => 6,
+		'incomeBga' => 6,
+		'missionIncome' => 7,
+		'wagePayment' => 5,
+		'other' => 8,
+		'loanInterest' => 9 
+
 );
-$financeHistory = array ();
+$summary = array (
+		'VEHICLES_NCS', // 0/ newVehiclesCost, soldVehicles, purchaseFuel
+		'LIFESTOCK_NSU', // 1/ newAnimalsCost, soldAnimals, animalUpkeep, soldWool, soldMilk
+		'PROPERTY_CSP', // 2/ constructionCost, soldBuildings, fieldPurchase
+		'VEHICLES_U', // 3/ vehicleRunningCost, vehicleLeasingCost
+		'PROPERTY_MI', // 4/ propertyMaintenance, propertyIncome
+		'WAGE_PAYMENT', // 5/ wagePayment
+		'HARVEST', // 6/ soldWood, soldBales, harvestIncome, incomeBga
+		'MISSION', // 7/ missionIncome
+		'OTHER', // 8/ purchaseSeeds, purchaseFertilizer, purchaseSaplings, purchaseWater, other
+		'LOANINTEREST' // 9/ loanInterest
+);
 $weekdays = array (
-		'',
+		'SUNDAY',
 		'MONDAY',
 		'TUESDAY',
 		'WEDNESDAY',
@@ -128,15 +59,21 @@ $mapping = array (
 		2,
 		1 
 );
+$financeHistory = $financeSummary = array ();
+$operatingResult = 0;
+
 foreach ( $careerEconomy->financeStatsHistory->financeStats as $financeStats ) {
 	$day = intval ( $financeStats ['day'] );
 	$financeHistory [$mapping [$day]] = array ();
 	foreach ( $financeElements as $element => $category ) {
 		$value = floatval ( $financeStats->$element );
+		$financeSummary [$summary [$category]] += $value;
+		$operatingResult += $value;
 		$financeHistory [$mapping [$day]] [$element] = round ( $value, 1, PHP_ROUND_HALF_UP );
 	}
 	$financeHistory [$mapping [$day]] ['total'] = array_sum ( $financeHistory [$mapping [$day]] );
 }
+$operatingResult = array_sum ( $financeSummary );
 $money = floatval ( $careerSavegame->statistics->money );
 $loan = floatval ( $careerSavegame->statistics->loan );
 $smarty->assign ( 'money', $money );
